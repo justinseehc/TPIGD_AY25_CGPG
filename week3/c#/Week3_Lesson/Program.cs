ec7321fb9c9e609819b560089a7e31cbee25d999
Justin <justinsee2004@gmail.com>
2025-10-24 16:46:17 +0800

    week 3

---
+++ week3/c#/Week3_Lesson/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+
+namespace Week3_Lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Vec3 Tests ===");
+
+            Vec3 a = new Vec3(1, 2, 3);
+            Vec3 b = new Vec3(4, 5, 6);
+            Vec3 c = new Vec3(2, 7, 1);
+
+            Console.WriteLine("A: " + a);
+            Console.WriteLine("B: " + b);
+            Console.WriteLine("C (Normalization): " + c);
+
+            // Test: Addition
+            //Vec3 add = a + b;
+            //Console.WriteLine($"a + b = {add} (Expected: 5,7,9)");
+            //Debug.Assert(add == new Vec3(5, 7, 9), "Addition failed");
+
+            // Test: Subtraction
+            //Vec3 sub = a - b;
+            //Console.WriteLine($"a - b = {sub} (Expected: -3,-3,-3)");
+            //Debug.Assert(sub == new Vec3(-3, -3, -3), "Subtraction failed");
+
+            // Test: Normalization
+            Vec3 norm = c / c;
+            Console.WriteLine($"c = {norm} (Expected: -3,-3,-3)");
+            //Debug.Assert(sub == new Vec3(-3, -3, -3), "Subtraction failed");
+        }
+    }
+}
---
+++ week3/c#/Week3_Lesson/Vec3.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_Lesson
+{
+    public class Vec3
+    {
+        private float x = 0f;
+        private float y = 0f;
+        private float z = 0f;
+        public Vec3() { }
+        public Vec3(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        // Operators
+        public static Vec3 operator +(Vec3 a, Vec3 b) // ADD OPERATOR
+        {
+            return new Vec3(a.x + b.x, a.y + b.y, a.z + b.z);
+        }
+
+        // TODO: Subtraction
+        public static Vec3 operator -(Vec3 a, Vec3 b) // SUBTRACT OPERATOR
+        {
+            return new Vec3(a.x - b.x, a.y - b.y, a.z - b.z);
+        }
+        // TODO: Normalisation
+        public static Vec3 operator /(Vec3 c) // NORMALISATION OPERATOR
+        {
+            private float mag = Math.Sqrt(  )
+            return new Vec3();
+        }
+        // TODO: Scalar Multiplication
+
+        public override string ToString() // To make it easier to print out stuff
+        {
+            return string.Format("({0:F3}, {1:F3}, {2:F3})", this.x, this.y, this.z);
+        }
+
+        // Equal Check
+        public static bool operator ==(Vec3 a, Vec3 b)
+        {
+            if (ReferenceEquals(a, b)) return true;
+            if (a is null || b is null) return false;
+
+            return a.x == b.x && a.y == b.y && a.z == b.z;
+        }
+        public static bool operator !=(Vec3 a, Vec3 b) => !(a == b);
+    }
+}
